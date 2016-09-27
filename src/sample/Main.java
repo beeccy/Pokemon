308afbd7358b878da5ffb7d5bb056193ed22c4f9
beeccy <beeccy3d@gmail.com>
2016-09-27 18:50:11 +0200

    Added three more Pokemon

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,9 +8,12 @@ public class Main {
         System.out.println("Hello from start");
 
         List<Pokemon> pokemon = Arrays.asList(
-                new Pokemon("Goomy"),
-                new Pokemon("Sliggoo"),
-                new Pokemon("Goodra")
+                new Pokemon("Goomy", "Dragon"),
+                new Pokemon("Sliggoo", "Dragon"),
+                new Pokemon("Goodra", "Dragon"),
+                new Pokemon("Pikachu", "Electric"),
+                new Pokemon("Raichu", "Electric"),
+                new Pokemon("Dedenne", "Electric, Fairy")
         );
 
         for (int i = 0; i < pokemon.size(); i++) {
@@ -32,17 +35,20 @@ class Pokemon {
     // dette er den som blir kjørt når du skriver "new Pokemon(...)"
     // denne heter konstruktøren fordi den "konstruerer"/lager pokemon
     public Pokemon(
-            String name
+            String name,
+            String type
     ) {
         // her kan du gjøre ting som skal gjøres når man skriver "new Pokemon(...)"
         // det er typisk å sette feltene lenger opp du vil gjøre
         this.name = name;
+        this.type = type;
     }
 
 
     // dette er en metode inni klassen
     public String toString() {
-        return "Name: " + this.name;
+        return "Name: " + this.name
+                + ", type: " + type;
     }
 
     // dette er en annen metode inni klassen
