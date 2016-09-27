ea7c299bf55926889a856a45c2ba6b42fbacd1c8
beeccy <beeccy3d@gmail.com>
2016-09-27 17:03:36 +0200

    Add some comments

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -15,18 +15,38 @@ public class Main {
 
         for (int i = 0; i < pokemon.size(); i++) {
             System.out.println(pokemon.get(i));
+
+            Pokemon currentPokemon = pokemon.get(i);
+            currentPokemon.sayHi(); // sånn her kaller du en metode
         }
     }
 }
 
-class Pokemon {
-    String name;
 
-    public Pokemon(String name) {
+class Pokemon {
+
+    // dette er ting en pokemon HAR (felter)
+    String name;
+    String type;
+
+    // dette er den som blir kjørt når du skriver "new Pokemon(...)"
+    // denne heter konstruktøren fordi den "konstruerer"/lager pokemon
+    public Pokemon(
+            String name
+    ) {
+        // her kan du gjøre ting som skal gjøres når man skriver "new Pokemon(...)"
+        // det er typisk å sette feltene lenger opp du vil gjøre
         this.name = name;
     }
 
+
+    // dette er en metode inni klassen
     public String toString() {
         return "Name: " + this.name;
+    }
+
+    // dette er en annen metode inni klassen
+    public void sayHi() {
+        System.out.println("Hi from " + name + "!");
     }
 }
